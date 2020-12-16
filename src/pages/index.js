097c4ae2c888da0c0a586f5823916390246fb2a3
Honorio <htaveras90@gmail.com>
2020-12-16 16:58:11 -0700

    added proptypes to project component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
-import Jobs from "../components/Jobs"
+// import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
-import Blogs from "../components/Blogs"
+// import Blogs from "../components/Blogs"
 
 export default ({ data }) => {
   const { allStrapiProjects: {

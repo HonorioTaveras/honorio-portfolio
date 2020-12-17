fe9cd465621372d1929b26f84d664e3b917452f9
Honorio <htaveras90@gmail.com>
2020-12-17 00:41:22 -0700

    added graphql query for about content type

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,28 @@ import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
 
-// ...GatsbyImageSharpFluid
+export const query = graphql`
+  {
+    allStrapiAbout {
+      nodes {
+        stack {
+          id
+          title
+        }
+        title
+        info
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 const About = () => {
   return <h2>about page</h2>
 }

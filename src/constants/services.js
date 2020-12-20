1623f8df1bf16258ba3fdd887069780b09fb5587
Honorio <htaveras90@gmail.com>
2020-12-19 21:25:45 -0700

    fixed wording services constant

--- src/constants/services.js
+++ src/constants/services.js
@@ -11,12 +11,12 @@ export default [
     id: 2,
     icon: <FaDatabase className="service-icon" />,
     title: "backend",
-    text: `I am comfortable designing and architecting clean, maintainable, and scalable back-ends using express/node.js to support high level web traffic using relational or non-relational databases like PostgresQL, MySQL, or MongoDB. I am able to containerize with Docker and deploy applications with AWS and I create the tests in order to ensure code integrity by using Jest and Enzyme or Mocha and Chai.`,
+    text: `I am comfortable designing and architecting clean, maintainable, and scalable back-ends using express/node.js to support high level web traffic using relational or non-relational databases like PostgresQL, MySQL, or MongoDB. I have containerized with Docker and deployed applications with AWS.`,
   },
   {
     id: 3,
     icon: <FaDev className="service-icon" />,
     title: "best practices",
-    text: `I have knowledge of professional software engineering practices & best practices for full software development life cycle, including coding standards, code reviews, source control management, test-driven development, continuous deployments, and agile methodologies`,
+    text: `I have knowledge of professional software engineering practices & best practices for full software development life cycle, including coding standards, code reviews, source control management, test-driven development in order to ensure code integrity by using Jest and Enzyme or Mocha and Chai, continuous deployments, and agile methodologies`,
   },
 ]

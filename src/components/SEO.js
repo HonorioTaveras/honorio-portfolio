9fb6b1c64bc43c61664ff9b2736351947bd78791
Honorio <htaveras90@gmail.com>
2020-12-19 19:15:35 -0700

    have graphql query in SEO component, accessing data

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -2,10 +2,36 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+const query = graphql`
+  {
+    site {
+      siteMetadata {
+        author
+        siteDesc: description
+        image
+        siteUrl
+        siteTitle: title
+        twitterUsername
+      }
+    }
+  }
+`
+
 const SEO = ({ title, description }) => {
-  return <Helmet htmlAttributes={{ lang: "en" }} title={title}>
-    <meta name="description" content={description} />
-  </Helmet>
+  const { site } = useStaticQuery(query)
+  const {
+    siteDesc,
+    siteTitle,
+    siteURL,
+    image,
+    twitterUsernme,
+  } = site.siteMetadata
+
+  return (
+    <Helmet htmlAttributes={{ lang: "en" }} title={title}>
+      <meta name="description" content={description} />
+    </Helmet>
+  )
 }
 
 export default SEO

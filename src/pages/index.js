a5efcccade60dadee220a0cb1b93ec5e360e961c
Honorio <htaveras90@gmail.com>
2020-12-20 19:04:24 -0700

    added particle effects to dependencies, still gotta find a way to make it work the way I want it to

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
+
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
 // import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 // import Blogs from "../components/Blogs"
-import SEO from '../components/SEO';
+import SEO from "../components/SEO"
 
 export default ({ data }) => {
-  const { allStrapiProjects: {
-    nodes: projects,
-  }} = data;
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
 
   return (
     <Layout>
